765bfeb7300330a9d93316da295c84125fb814c9
jobukk <jobukipkurui3843@gmail.com>
2024-01-06 20:29:46 +0300

    added .decode attribute to image tag

--- src/index.js
+++ src/index.js
@@ -46,14 +46,17 @@ for (let i = 0; i<logoo.length; i++){
 let groupp = document.getElementById("group")
 console.log(groupp)
 groupp.src = group;
+groupp.decoding = "async"
 
 let vectorr = document.getElementById("vector")
 console.log(vectorr)
 vectorr.src = vector;
+vectorr.decoding = "async"
 
 let cartt = document.getElementById("cart")
 console.log(cartt)
 cartt.src = cart;
+cartt.decoding = "async"
 
 let signInn = document.querySelectorAll(".signIn")
 console.log(signInn)
@@ -62,6 +65,7 @@ signInn = Array.from(signInn)
 for (let j = 0; j<signInn.length; j++){
   console.log(signInn[j])
   signInn[j].src = signIn
+  signInn[j].decoding = "async"
 }
 
 
@@ -69,68 +73,89 @@ for (let j = 0; j<signInn.length; j++){
 let menuu = document.getElementById("menu")
 console.log(menuu)
 menuu.src = menu;
+menuu.decoding = "async"
 
 let herosvgg = document.getElementById("herosvg")
 console.log(herosvgg)
 herosvgg.src = imgHero;
+herosvgg.decoding = "async"
 
 let ordersvgg = document.getElementById("ordersvg")
 console.log(ordersvgg)
 ordersvgg.src = order;
+ordersvgg.decoding = "async"
 
 let deliverysvgg = document.getElementById("deliverysvg")
 console.log(deliverysvgg)
 deliverysvgg.src = delivery;
+deliverysvgg.decoding = "async"
 
 let couriersvgg = document.getElementById("couriersvg")
 console.log(couriersvgg)
 couriersvgg.src = courier;
+couriersvgg.decoding = "async"
 
 let unsplash_GG = document.getElementById("unsplash_G")
 console.log(unsplash_GG)
 unsplash_GG.src = unsplashG
+unsplash_GG.decoding = "async"
 
 let unsplash_zz = document.getElementById("unsplash_z")
 console.log(unsplash_zz)
 unsplash_zz.src = unsplashz
+unsplash_zz.decoding = "async"
 
 let top11 = document.querySelectorAll(".top1")
 console.log(top11)
 top11.src = top
+top11.decoding = "async"
 
 for (let i = 0; i<top11.length; i++){
   console.log(top11[i])
   top11[i].src=topcoatLike1;
+  top11[i].decoding = "async"
 }
 let unsplash_SS = document.getElementById("unsplash_S")
 console.log(unsplash_SS)
 unsplash_SS.src = unsplashS;
+unsplash_SS.decoding = "async"
 
 let unsplash_vv = document.getElementById("unsplash_v")
 console.log(unsplash_vv)
 unsplash_vv.src = unsplashv;
+unsplash_vv.decoding = "async"
 
 let unsplash_YY = document.getElementById("unsplash_Y")
 console.log(unsplash_YY)
 unsplash_YY.src = unsplashY;
+unsplash_YY.decoding = "async"
+
 
 let unsplash_KK = document.getElementById("unsplash_K")
 console.log(unsplash_KK)
 unsplash_KK.src = unsplashK;
+unsplash_KK.decoding = "async"
+
 
 let unsplash_MM = document.getElementById("unsplash_M")
 console.log(unsplash_MM)
 unsplash_MM.src = unsplashM;
+unsplash_MM.decoding = "async"
+
 
 let unsplash_XX = document.getElementById("unsplash_X")
 console.log(unsplash_XX)
 unsplash_XX.src = unsplashX;
+unsplash_XX.decoding = "async"
+
 
 let design_starr = document.querySelectorAll(".design_star")
 console.log(design_starr)
 design_starr = Array.from(design_starr)
 for (let i = 0; i<design_starr.length; i++){
   design_starr[i].src =  star
+  design_starr[i].decoding = "async"
+
 }
 
 let bxs_starr = document.querySelectorAll(".bxs-star")
@@ -138,6 +163,8 @@ console.log(bxs_starr)
 bxs_starr = Array.from(bxs_starr)
 for (let i = 0; i<bxs_starr.length; i++){
   bxs_starr[i].src =  starHalf
+  bxs_starr[i].decoding = "async"
+
 }
 
 let unsplash_X11 = document.querySelectorAll(".unsplash_X1")
@@ -145,25 +172,35 @@ console.log(unsplash_X11)
 unsplash_X11 = Array.from(unsplash_X11);
 for (let i = 0; i<unsplash_X11.length; i++){
   unsplash_X11[i].src = unsplashX1
+  unsplash_X11[i].decoding = "async"
+
 }
 
 let iconsinstagram = document.getElementById("icons_instagram")
 console.log(iconsinstagram)
 iconsinstagram.src = instagram;
+iconsinstagram.decoding = "async"
+
 
 let bxlfacebook = document.getElementById("bxl-facebook")
 console.log(bxlfacebook)
 bxlfacebook.src = facebook;
+bxlfacebook.decoding = "async"
+
 
 let twitterr = document.getElementById("twitter")
 console.log(twitterr)
 twitterr.src = twitter
+twitterr.decoding = "async"
+
 
 
 let sectionFive =document.querySelector(".page-five");
 console.log(sectionFive)
 let image = new Image();
 image.src = unsplash2;
+image.decoding = "async"
+
 image.onload = function() {
   sectionFive.style.backgroundImage = `url(${unsplash2})`;
 };
@@ -172,6 +209,8 @@ let hero =document.querySelector(".hero");
 console.log(hero)
 let img = new Image();
 img.src = shape;
+img.decoding = "async"
+
 img.onload = function() {
   hero.style.backgroundImage = `url(${shape})`;
 };
@@ -180,6 +219,8 @@ let footer =document.getElementById("f");
 console.log(footer)
 let fimg = new Image();
 fimg.src = Bg;
+fimg.decoding = "async"
+
 fimg.onload = function() {
   footer.style.backgroundImage = `url(${Bg})`;
 };
